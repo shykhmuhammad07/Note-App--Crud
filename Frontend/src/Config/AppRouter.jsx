7b9533eb326f370315fdab0f1a6542e1a8d3a82a
shykhmuhammad07 <muhammadsheikh880@gmail.com>
2025-11-17 19:55:25 +0500

    la

--- Frontend/src/Config/AppRouter.jsx
+++ Frontend/src/Config/AppRouter.jsx
@@ -13,6 +13,7 @@ function AppRouter() {
     <Navbar />
     <Routes>
       <Route path='/' element ={<Home />} />
+      <Route path='/landing' element ={<Home />} />
       <Route path='/signup' element ={<Signup />} />
       <Route path='/login' element ={<Login />} />
       <Route path='/create' element ={<Create />} />
